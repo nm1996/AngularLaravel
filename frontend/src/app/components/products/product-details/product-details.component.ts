7e49ef3254bdbc46841405575f36e6389284279c
Nikola <nikola.milanovic.118.15@ict.edu.rs>
2019-09-10 18:13:25 +0200

    new template

--- frontend/src/app/components/products/product-details/product-details.component.ts
+++ frontend/src/app/components/products/product-details/product-details.component.ts
@@ -31,7 +31,7 @@ export class ProductDetailsComponent implements OnInit {
     private token: TokenService,
     private http: HttpClient
   ) {
-    this.numbers = [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46];
+    this.numbers = [34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46];
   }
 
   details;
